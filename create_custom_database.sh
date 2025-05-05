9d7e352fe80a8b5cf94014af551314596e21bef2
jirkavlasak <jirivlasak08@gmail.com>
2025-05-05 14:15:59 +0200

    Create create_custom_database.sh

---
+++ create_custom_database.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DB_DIR=vlasak_database
+SAMPLES_DIR=/home/jirka_vlasak/detaxizer_pipeline/samples_custom_db
+
+echo "[$(date)] Starting: Downloading taxonomy"
+kraken2-build --download-taxonomy --db $DB_DIR
+
+echo "[$(date)] Adding all .fna files from $SAMPLES_DIR"
+for file in $SAMPLES_DIR/*.fna; do
+    kraken2-build --add-to-library $file --db $DB_DIR
+done
+
+echo "[$(date)] Adding human reference"
+kraken2-build --download-library human --db $DB_DIR
+
+echo "[$(date)] Building the Kraken2 database"
+kraken2-build --build --db $DB_DIR
+
+echo "[$(date)] Cleaning temporary files"
+kraken2-build --clean --db $DB_DIR
+
+echo "[$(date)] All steps completed"
